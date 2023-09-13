6b741e7a698fac4cfc002d5fa1214a32653e5399
zeeshanafzal73~ <zeeshanafzal73>
2023-09-14 01:44:16 +0500

    Token authentication done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./components/Pages/Home";
 import { Footer } from "./components/Footer";
 import { About } from "./components/Pages/About";
@@ -10,56 +10,42 @@ import { Contact } from "./components/Pages/Contact";
 import { Search } from "react-bootstrap-icons";
 import { Login } from "./components/Pages/Login";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-    };
-  }
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
 
   // Function to handle login
-  handleLogin = () => {
-    this.setState({ isLoggedIn: true });
+  const handleLogin = () => {
+    setIsLoggedIn(localStorage.getItem('token'));
   };
 
   // Function to handle logout
-  handleLogout = () => {
-   
-      alert('Logged-out');
-    
-    
+  const handleLogout = () => {
     localStorage.removeItem("token");
-    this.setState({ isLoggedIn: false });
+    setIsLoggedIn(null);
   };
 
-  render() {
-    const { isLoggedIn } = this.state;
+  // Use useEffect to check token status when the app loads
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(token);
+  }, []);
 
-    return (
-      <>
-        <Router>
-          {isLoggedIn && <Navbar onLogout={this.handleLogout} />}
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Login
-                  onLogin={this.handleLogin}
-                  isLoggedIn={isLoggedIn} // Pass the isLoggedIn state to the Login component
-                />
-              }
-            />
-            {isLoggedIn && <Route path="/home" element={<Home />} />}
-            <Route path="/about" element={<About />} />
-            <Route path="/property" element={<Property />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/search" element={<Search />} />
-          </Routes>
-          {isLoggedIn && <Footer />}
-        </Router>
-      </>
-    );
-  }
+  return (
+    <Router>
+      {isLoggedIn && <Navbar onLogout={handleLogout} />}
+      <Routes>
+        <Route
+          path="/"
+          element={isLoggedIn ? <Navigate to="/home" /> : <Login onLogin={handleLogin} />}
+        />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/property" element={<Property />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+      {isLoggedIn && <Footer />}
+    </Router>
+  );
 }
--- src/components/LatestProperties.jsx
+++ src/components/LatestProperties.jsx
@@ -1,112 +1,11 @@
-import React, { useState } from "react";
-import "./Navbar/Navbar.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Carousel } from "react-bootstrap";
-import property1 from "./Static/img/property-1.jpg";
-import property2 from "./Static/img/property-2.jpg";
-import property3 from "./Static/img/property-3.jpg";
-import property4 from "./Static/img/property-4.jpg";
-import property5 from "./Static/img/property-5.jpg";
-import property6 from "./Static/img/property-6.jpg";
-import property7 from "./Static/img/property-7.jpg";
-import property8 from "./Static/img/property-8.jpg";
-import property9 from "./Static/img/property-9.jpg";
-import property10 from "./Static/img/property-10.jpg";
 
 
-const propertyData = [
-  {
-    imageSrc: property1,
-    title: "206 Mount Olive Road Two",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-  {
-    imageSrc: property2,
-    title: "157 West Central Park",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-  {
-    imageSrc: property3,
-    title: "245 Azabu Nishi Park let",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-  {
-    imageSrc: property4,
-    title: "204 Montal South Bela Two",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-  {
-    imageSrc: property5,
-    title: "204 Montal South Bela Two",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-  {
-    imageSrc: property6,
-    title: "204 Montal South Bela Two",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-  {
-    imageSrc: property7,
-    title: "204 Montal South Bela Two",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-  {
-    imageSrc: property8,
-    title: "204 Montal South Bela Two",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-  {
-    imageSrc: property9,
-    title: "204 Montal South Bela Two",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-  {
-    imageSrc: property10,
-    title: "204 Montal South Bela Two",
-    price: "rent | $ 12.000",
-    area: "340m²",
-    beds: "2",
-    baths: "4",
-    garages: "1",
-  },
-];
 
 export const LatestProperties = () => {
+    const [propertyData, setPropertyData] = useState([]);
     const itemsPerSlide = 3; // Number of items per slide
   
     // Split property data into groups of itemsPerSlide
@@ -114,12 +13,25 @@ export const LatestProperties = () => {
     for (let i = 0; i < propertyData.length; i += itemsPerSlide) {
       groupedPropertyData.push(propertyData.slice(i, i + itemsPerSlide));
     }
-  
+    
     const [index, setIndex] = useState(0);
   
     const handleSelect = (selectedIndex, e) => {
       setIndex(selectedIndex);
     };
+     
+     // Use useEffect to fetch data when the component mounts
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const res = await axios.get("http://127.0.0.1:8000/property/");
+          setPropertyData(res.data); 
+        } catch (err) {
+          console.log("error", err);
+        }
+      };
+      fetchData();
+    }, []);
   
     return (
       <>
@@ -146,7 +58,7 @@ export const LatestProperties = () => {
                     <div className="card-box-a card-shadow">
                       <div className="img-box-a">
                         <img
-                          src={property.imageSrc}
+                          src={property.image}
                           alt=""
                           className="img-a img-fluid"
                         />
@@ -160,7 +72,7 @@ export const LatestProperties = () => {
                           </div>
                           <div className="card-body-a">
                             <div className="price-box d-flex">
-                              <span className="price-a">{property.price}</span>
+                              <span className="price-a">rent | $ {property.price}</span>
                             </div>
                             <a href="#" className="link-a">
                               Click here to view
@@ -171,7 +83,7 @@ export const LatestProperties = () => {
                             <ul className="card-info d-flex justify-content-around">
                               <li>
                                 <h4 className="card-info-title">Area</h4>
-                                <span>{property.area}</span>
+                                <span>{property.area}m²</span>
                               </li>
                               <li>
                                 <h4 className="card-info-title">Beds</h4>
